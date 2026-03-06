5902e8d6cdd957c0465773275b6958f0a1802dad
bittu7647 <bittusharma230305@gmail.com>
2026-03-07 01:45:05 +0530

    Added ESP32 WiFi connection code

--- hardware/esp32_main/main/main.ino
+++ hardware/esp32_main/main/main.ino
@@ -1,8 +1,19 @@
+#include <WiFi.h>
+
+const char* ssid = "iqoo1234";
+const char* password = "hello123";
+
 void setup() {
   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+
+  Serial.println("WiFi Connected");
 }
 
-void loop() {
-  Serial.println("ESP32 Working");
-  delay(2000);
-}
+void loop(){}
