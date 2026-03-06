0213843217a6466515d8c085b72e7ddd9d6c2320
bipladipsaha <bipladip555@gmail.com>
2026-03-07 02:44:06 +0530

    Added GPS location tracking using NEO-6M module

--- hardware/esp32_main/main/main.ino
+++ hardware/esp32_main/main/main.ino
@@ -2,32 +2,31 @@
 #include <TinyGPS++.h>
 #include <HardwareSerial.h>
 
-// WiFi
 const char* ssid = "iqoo1234";
 const char* password = "hello123";
 
-// GPS
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(2);
+
+#define SOS_BUTTON 14
+
+bool trackingActive = false;
 
 void setup() {
 
   Serial.begin(115200);
 
-  // WiFi
-  Serial.println("Connecting to WiFi...");
+  pinMode(SOS_BUTTON, INPUT_PULLUP);
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting...");
+    delay(500);
   }
 
   Serial.println("WiFi Connected");
 
-  // GPS
   gpsSerial.begin(9600, SERIAL_8N1, 16, 17);
-  Serial.println("GPS Module Initialized");
 }
 
 void loop() {
@@ -36,14 +35,24 @@ void loop() {
     gps.encode(gpsSerial.read());
   }
 
-  if (gps.location.isUpdated()) {
+  if (digitalRead(SOS_BUTTON) == LOW) {
+
+    trackingActive = !trackingActive;
+    delay(500);
+
+    if (trackingActive) {
+      Serial.println("SOS Activated");
+    } else {
+      Serial.println("Tracking Stopped");
+    }
+  }
+
+  if (trackingActive && gps.location.isValid()) {
 
     Serial.print("Latitude: ");
     Serial.println(gps.location.lat(), 6);
 
     Serial.print("Longitude: ");
     Serial.println(gps.location.lng(), 6);
-
-    Serial.println("-------------------");
   }
 }
